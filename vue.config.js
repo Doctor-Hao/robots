be665a0782c43d25b9d5a975abff3da5660ea262
Meng H00 <zhmuruk1@gmail.com>
2024-04-13 12:12:32 +0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/robots/'
-    : '/',
-  transpileDependencies: true
+    : '/'
 })
